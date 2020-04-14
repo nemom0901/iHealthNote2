4f17ef878f2a48ad423dd0b707916f774c06e1f0
judy <>
2020-04-14 16:22:16 +0800

    no message

--- iHealthNote/ViewController.swift
+++ iHealthNote/ViewController.swift
@@ -13,8 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //變數
+        var index : Int = 1
+        
+        //常數
+        let opcode : Int = 2
+        
+        let result = index + opcode
+        
+        print("result:\( result )")
+        
+        var 購買橘子的數量 : Int = 0
+        var 購買西瓜的數量 = 0
+        
+        let 我去超級市場 : Bool = true
+        let 我看到西瓜 = true
+        
+        if 我去超級市場 {
+            if 我看到西瓜 {
+                購買橘子的數量 = 3
+                購買西瓜的數量 = 1
+                
+            }else{
+                購買橘子的數量 = 3
+            }
+        }
+        }
+    
+    override func viewWillAppear (_ animated: Bool){
+    
+    }
+        
+        
     }
 
-
-}
 
